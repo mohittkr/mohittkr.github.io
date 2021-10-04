d0e98747328e075afc3f7bfc2742348ff20d93e6
Mohit Tekriwal <tmohit@citabria.local>
2021-10-04 19:10:10 -0400

    fixed typo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const tabs = document.querySelectorAll('[data-tab-target')
-const tabContents = document.querySelectorAll('[data-tab-content')
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
